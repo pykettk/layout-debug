b91cff2f92482a467e9d40462b39efbd841b1b04
Kiel Pykett <pykettk@gmail.com>
2020-01-01 06:37:49 +0000

    Log Compiled Layout XML
    + Create an observer for the frontend 'layout_generate_blocks_after' event that logs the compiled layout XML

---
+++ Observer/LayoutDebug.php
@@ -0,0 +1,24 @@
+<?php
+namespace PykettK\LayoutDebug\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Zend\Log\Logger;
+use Zend\Log\Writer\Stream;
+
+class LayoutDebug implements ObserverInterface
+{
+    /**
+     * @param Observer $observer
+     * @return $this|void
+     */
+    public function execute(Observer $observer)
+    {
+        $logger = new Logger();
+
+        $logger->addWriter(new Stream(BP . '/var/log/layout.log'));
+        $logger->info($observer->getEvent()->getLayout()->getXmlString());
+
+        return $this;
+    }
+}
